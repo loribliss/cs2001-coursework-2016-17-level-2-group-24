f39078065143f51dcb2e0bc41e3bc47d2db84da9
FrankieKhor <frankiekhor12344321@gmail.com>
2017-02-14 15:28:32 +0000

    Fixed some probelms with implmentaion of google map, updated scrum sheet and added jdbc for the database

--- FoodManagement/app/src/main/java/com/example/cs15fmk/foodmanagement/SignUpActivity.java
+++ FoodManagement/app/src/main/java/com/example/cs15fmk/foodmanagement/SignUpActivity.java
@@ -5,20 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
+import android.widget.ViewSwitcher;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 import static com.example.cs15fmk.foodmanagement.R.id.btn_signup;
+import static com.example.cs15fmk.foodmanagement.R.id.signOut;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText editName, editEmail, editPassword;
-
+    ViewSwitcher simpleViewSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+         simpleViewSwitcher = (ViewSwitcher) findViewById(R.id.view_switcher); // get the reference of ViewSwitcher
         Button btn = (Button) findViewById(btn_signup);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,8 +31,12 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
                 Toast.makeText(SignUpActivity.this,
                         "You have signed up", Toast.LENGTH_SHORT).show();
+/*simpleViewSwitcher.showNext();*/
             }
         });
+    }
+    public void onClickSignOut(){
+       /* simpleViewSwitcher.showPrevious();*/
     }
 
 
--- FoodManagement/app/src/main/java/layout/FoodCupboardFragment.java
+++ FoodManagement/app/src/main/java/layout/FoodCupboardFragment.java
@@ -22,8 +22,10 @@ public class FoodCupboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_food_cupboard, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_cupboard, container, false);
+        return v;
+
     }
 
 }
