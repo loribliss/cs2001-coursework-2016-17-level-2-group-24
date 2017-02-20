d21bb184bed6039ce66bc28159f8f9e1347180c8
Sim Sohi <simsohi@Sims-MacBook-Pro.local>
2017-02-20 11:52:41 +0000

    changes made to recipies and ingredients

--- FoodManagement/app/src/main/java/layout/FindFoodFragment.java
+++ FoodManagement/app/src/main/java/layout/FindFoodFragment.java
@@ -284,9 +284,9 @@ public class FindFoodFragment extends Fragment implements OnMapReadyCallback,
     }
 
     public void onStop() {
+        super.onStop();
         if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
-            super.onStop();
         }
     }
 
--- FoodManagement/app/src/main/java/layout/RecipesFragment.java
+++ FoodManagement/app/src/main/java/layout/RecipesFragment.java
@@ -1,12 +1,20 @@
 package layout;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.SeekBar;
 
+import com.example.cs15fmk.foodmanagement.AddNewFCItemMenu;
 import com.example.cs15fmk.foodmanagement.MainActivity;
 import com.example.cs15fmk.foodmanagement.R;
 
@@ -15,6 +23,13 @@ import com.example.cs15fmk.foodmanagement.R;
  */
 public class RecipesFragment extends Fragment {
 
+    private Activity activity;
+
+    SeekBar timescale;
+    SearchView foodTypes;
+    ListView ingredients;
+    SearchView cuisineSearch;
+    Button okay;
 
     public RecipesFragment() {
         // Required empty public constructor
@@ -26,7 +41,26 @@ public class RecipesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_recipes, container, false);
+        activity = getActivity();
+        View view = inflater.inflate(R.layout.fragment_recipes, container, false);
+
+        timescale = (SeekBar) view.findViewById(R.id.timescale);
+        foodTypes = (SearchView) view.findViewById(R.id.foodTypes);
+        ingredients = (ListView) view.findViewById(R.id.ingredients);
+        cuisineSearch = (SearchView) view.findViewById(R.id.cuisineSearch);
+        okay = (Button) view.findViewById(R.id.okay);
+
+
+        okay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+
+        return view;
     }
 
 }
